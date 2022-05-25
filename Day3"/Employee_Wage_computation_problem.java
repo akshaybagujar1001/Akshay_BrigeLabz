87f50c56301e7ce265828069eadb4ea33d0096be
Akshay Badgujar <104914967+akshaybagujar1001@users.noreply.github.com>
2022-05-25 15:56:36 +0530

    Add files via upload

---
+++ Day3"/Employee_Wage_computation_problem.java
@@ -0,0 +1,11 @@
+package Day3praciseproblem;
+
+
+
+public class Employee_Wage_computation_problem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+
+
+}
