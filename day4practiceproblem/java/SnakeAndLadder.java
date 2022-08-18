966faf01a16dc8953f5e73da011115701216f5fa
Akshay Badgujar <104914967+akshaybagujar1001@users.noreply.github.com>
2022-08-18 12:41:58 +0530

    Add files via upload

---
+++ day4practiceproblem/java/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.day4practiceproblem.java;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        int dieRoll=(int)(Math.random()*7);
+        System.out.println(dieRoll);
+    }
+}
